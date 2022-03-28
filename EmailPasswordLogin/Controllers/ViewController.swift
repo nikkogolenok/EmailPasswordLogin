7ec92c15ab545119ff97c8168d82d272923f46b3
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-28 14:21:26 +0300

    Initial Commit

---
+++ EmailPasswordLogin/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  EmailPasswordLogin
+//
+//  Created by Никита Коголенок on 28.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
